8d3a2fa30aa075a9d75756d45da38fa689a1378d
Zacchary Puckeridge <zacchary@puckeridge.me>
2021-05-11 21:08:19 +1000

    Updated Front Page Text Formatting. Added Social Buttons. Updated Formatting.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import * as React from "react"
 
-import { Helmet } from "react-helmet"
 import { graphql } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { Helmet } from "react-helmet"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -14,13 +13,51 @@ const HomePage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="Home | Zacchary Puckeridge" />
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        />
+      </Helmet>
       <div class="outer-container">
-          <div class="home-container">
-              <p class="name-header">Zacchary Puckeridge</p>
-              <Bio />
+        <div class="home-container">
+          <p class="name-header">Zacchary Puckeridge</p>
+          <Bio />
+          <div class="social-icons">
+            <div>
+              <table>
+                <tr>
+                  <th>
+                    <a href="https://github.com/zpuckeridge">
+                      <span class="social-icon">
+                        <i class="fab fa-github-square"></i>
+                      </span>
+                    </a>
+                  </th>
+                  <th>
+                    <a href="https://www.linkedin.com/in/zpuckeridge/">
+                      <span class="social-icon">
+                        <i class="fab fa-linkedin"></i>
+                      </span>
+                    </a>
+                  </th>
+                  <th>
+                    <a href="/blog/">
+                      <span class="social-icon">
+                        <i class="fas fa-rss-square"></i>
+                      </span>
+                    </a>
+                  </th>
+                </tr>
+              </table>
+            </div>
           </div>
+          <div class="description">
+            <a href="/about/">Click here to learn more about me!</a>
+          </div>
+        </div>
       </div>
-      </Layout>
+    </Layout>
   )
 }
 
