4d55829ece69354289426eee59d3931603b0e3c1
Zacchary Puckeridge <zacchary@puckeridge.me>
2021-08-01 08:31:15 +0000

    Added new project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -33,6 +33,63 @@ export default function Projects() {
         </VStack>
       </Box>
       <Box marginTop="50px">
+      <SimpleGrid
+          alignItems="center"
+          columns={{ base: 1, md: 2 }}
+          flexDirection="column-reverse"
+          mb={24}
+          spacingY={{ base: 10, md: 32 }}
+          spacingX={{ base: 10, md: 10 }}
+        >
+          <Box order={{ base: 'none', md: 2 }}>
+            <Link
+              href={`https://savvyelectricalanddata.com.au`}
+              mb={4}
+              fontSize={{ base: '2xl', md: '4xl' }}
+              fontWeight="extrabold"
+              letterSpacing="tight"
+              textAlign={{ base: 'center', md: 'left' }}
+              lineHeight={{ md: 'shorter' }}
+              _hover={{
+                color: '#30ce56',
+                textDecor: 'none',
+              }}
+            >
+              Savvy Electrical and Data
+              <Icon
+                marginLeft="0.5rem"
+                w={6}
+                h={6}
+                as={ExternalLinkIcon}
+              />
+            </Link>
+            <Text
+              mb={5}
+              textAlign={{ base: 'center', sm: 'left' }}
+              fontSize={{ md: 'lg' }}
+              color="#c3c8d3"
+            >
+              Savvy Electrical and Data needed a simple site to get their business up and running on the web.
+            </Text>
+          </Box>
+          <Center>
+            <Box
+              boxShadow="0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);"
+              borderRadius="10px"
+              width={300}
+            >
+              <StaticImage
+                src="../../src/images/website-preview-savvy.png"
+                alt="Zacchary Puckeridge"
+                placeholder="blurred"
+                width={300}
+                height={225}
+                quality={100}
+                style={{ borderRadius: `10px` }}
+              />
+            </Box>
+          </Center>
+        </SimpleGrid>
         <SimpleGrid
           alignItems="start"
           columns={{ base: 1, md: 2 }}
