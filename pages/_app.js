97a1ec842ea2a5d56b9f8a6ca539ceca7b9f7c7d
Zacchary Puckeridge <zacchary@puckeridge.me>
2022-07-18 17:04:38 +1000

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+// pages/_app.js
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
