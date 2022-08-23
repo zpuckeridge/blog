c92c5000d3502253b61401660808c10eec38c517
zpuckeridge <zacchary@puckeridge.me>
2022-08-23 07:23:30 +0000

    Fixed Types

--- pages/about.tsx
+++ pages/about.tsx
@@ -35,144 +35,16 @@ export default function About() {
               </h2>
             </div>
           </div>
-
-          <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
-            <ul
-              className="flex flex-wrap -mb-px text-sm font-medium text-center"
-              id="myTab"
-              data-tabs-toggle="#myTabContent"
-              role="tablist"
-            >
-              <li className="mr-2" role="presentation">
-                <button
-                  className="inline-block p-4 rounded-t-lg border-b-2 text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500"
-                  id="profile-tab"
-                  data-tabs-target="#profile"
-                  type="button"
-                  role="tab"
-                  aria-controls="profile"
-                  aria-selected="true"
-                >
-                  Profile
-                </button>
-              </li>
-              <li className="mr-2" role="presentation">
-                <button
-                  className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700"
-                  id="dashboard-tab"
-                  data-tabs-target="#dashboard"
-                  type="button"
-                  role="tab"
-                  aria-controls="dashboard"
-                  aria-selected="false"
-                >
-                  Dashboard
-                </button>
-              </li>
-              <li className="mr-2" role="presentation">
-                <button
-                  className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700"
-                  id="settings-tab"
-                  data-tabs-target="#settings"
-                  type="button"
-                  role="tab"
-                  aria-controls="settings"
-                  aria-selected="false"
-                >
-                  Settings
-                </button>
-              </li>
-              <li role="presentation">
-                <button
-                  className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 dark:border-transparent text-gray-500 dark:text-gray-400 border-gray-100 dark:border-gray-700"
-                  id="contacts-tab"
-                  data-tabs-target="#contacts"
-                  type="button"
-                  role="tab"
-                  aria-controls="contacts"
-                  aria-selected="false"
-                >
-                  Contacts
-                </button>
-              </li>
-            </ul>
+          <div className="grid place-items-center h-64 w-64">
+            <Image
+              alt="Zacchary Puckeridge"
+              width={100}
+              height={100}
+              priority
+              src="/images/profile-pic.jpg"
+              className="w-full shadow-xl rounded-xl"
+            />
           </div>
-          <div id="myTabContent">
-            <div
-              className="p-4 bg-gray-50 rounded-lg dark:bg-gray-800"
-              id="profile"
-              role="tabpanel"
-              aria-labelledby="profile-tab"
-            >
-              <p className="text-sm text-gray-500 dark:text-gray-400">
-                This is some placeholder content the{" "}
-                <strong className="font-medium text-gray-800 dark:text-white">
-                  Profile tab's associated content
-                </strong>
-                . Clicking another tab will toggle the visibility of this one
-                for the next. The tab JavaScript swaps classes to control the
-                content visibility and styling.
-              </p>
-            </div>
-            <div
-              className="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800"
-              id="dashboard"
-              role="tabpanel"
-              aria-labelledby="dashboard-tab"
-            >
-              <p className="text-sm text-gray-500 dark:text-gray-400">
-                This is some placeholder content the{" "}
-                <strong className="font-medium text-gray-800 dark:text-white">
-                  Dashboard tab's associated content
-                </strong>
-                . Clicking another tab will toggle the visibility of this one
-                for the next. The tab JavaScript swaps classes to control the
-                content visibility and styling.
-              </p>
-            </div>
-            <div
-              className="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800"
-              id="settings"
-              role="tabpanel"
-              aria-labelledby="settings-tab"
-            >
-              <p className="text-sm text-gray-500 dark:text-gray-400">
-                This is some placeholder content the{" "}
-                <strong className="font-medium text-gray-800 dark:text-white">
-                  Settings tab's associated content
-                </strong>
-                . Clicking another tab will toggle the visibility of this one
-                for the next. The tab JavaScript swaps classes to control the
-                content visibility and styling.
-              </p>
-            </div>
-            <div
-              className="hidden p-4 bg-gray-50 rounded-lg dark:bg-gray-800"
-              id="contacts"
-              role="tabpanel"
-              aria-labelledby="contacts-tab"
-            >
-              <p className="text-sm text-gray-500 dark:text-gray-400">
-                This is some placeholder content the{" "}
-                <strong className="font-medium text-gray-800 dark:text-white">
-                  Contacts tab's associated content
-                </strong>
-                . Clicking another tab will toggle the visibility of this one
-                for the next. The tab JavaScript swaps classes to control the
-                content visibility and styling.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="grid place-items-center h-64 w-64">
-          <Image
-            alt="Zacchary Puckeridge"
-            width={100}
-            height={100}
-            priority
-            src="/images/profile-pic.jpg"
-            className="w-full shadow-xl rounded-xl"
-          />
         </div>
       </div>
     </>
