b8547de4e4010742f45bd6d774579947bd946e39
zpuckeridge <zacchary@puckeridge.me>
2023-07-28 00:45:59 +0000

    fix: added `/about` to `publicRoutes`

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
-  afterAuth(auth, req, evt) {
+  afterAuth(auth, req) {
     if (!auth.userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: req.url });
     }
@@ -10,6 +10,7 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/blog",
+    "/about",
     "/(article)(.*)",
     "/unauthorised",
     "/sign-in",
