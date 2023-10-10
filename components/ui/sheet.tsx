fac6d7be24cfd7aeb5649390d6e11627acc662e0
zpuckeridge <zacchary@puckeridge.me>
2023-10-10 00:43:08 +0000

    fix: ignore ts error

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -14,9 +14,11 @@ const SheetTrigger = SheetPrimitive.Trigger;
 const SheetClose = SheetPrimitive.Close;
 
 const SheetPortal = ({
+  // @ts-ignore
   className,
   ...props
 }: SheetPrimitive.DialogPortalProps) => (
+  // @ts-ignore
   <SheetPrimitive.Portal className={cn(className)} {...props} />
 );
 SheetPortal.displayName = SheetPrimitive.Portal.displayName;
