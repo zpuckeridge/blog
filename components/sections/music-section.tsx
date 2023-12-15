5147cc260439a6f9c9b3b0c2edfc4831fab8e76a
zpuckeridge <zacchary@puckeridge.me>
2023-12-15 01:31:01 +0000

    refactor: music-section

--- components/sections/music-section.tsx
+++ components/sections/music-section.tsx
@@ -1,246 +1,122 @@
 import { getAccessToken } from "@/lib/spotify";
 import Image from "next/image";
 
-async function getCurrentTopTracks() {
-  const { access_token } = await getAccessToken();
+async function getTopTracks(time_range: string) {
+  try {
+    const { access_token } = await getAccessToken();
 
-  const res = await fetch(
-    `https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=5`,
-    {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
+    const res = await fetch(
+      `https://api.spotify.com/v1/me/top/tracks?time_range=${time_range}&limit=5`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
       },
-    },
-  );
+    );
 
-  if (!res.ok) {
-    throw new Error("Error fetching current top tracks");
+    if (!res.ok) {
+      if (res.status === 401) {
+        throw new Error("Unauthorized access. Please check your access token.");
+      } else {
+        throw new Error(
+          "Error fetching last played data. Status:" + res.status,
+        );
+      }
+    }
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching top tracks data:", error);
+    throw error;
   }
-
-  const data = await res.json();
-
-  return data;
 }
 
-async function getRecentTopTracks() {
-  const { access_token } = await getAccessToken();
-
-  const res = await fetch(
-    `https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=5`,
-    {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    },
-  );
-
-  if (!res.ok) {
-    throw new Error("Error fetching recent top tracks");
-  }
-
-  const data = await res.json();
-
-  return data;
-}
-
-async function getAllTimeTopTracks() {
-  const { access_token } = await getAccessToken();
-
-  const res = await fetch(
-    `https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=5`,
-    {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    },
-  );
-
-  if (!res.ok) {
-    throw new Error("Error fetching all time top tracks");
-  }
-
-  const data = await res.json();
-
-  return data;
-}
+const Track = ({ track, index }: { track: any; index: number }) => (
+  <div
+    key={index}
+    className="hover:bg-muted py-1 px-2 transition-all duration-200"
+  >
+    <div className="flex gap-4">
+      <div className="text-muted-foreground my-auto whitespace-nowrap w-2">
+        {index + 1}
+      </div>
+      <Image
+        src={track.album.images[0].url}
+        alt={track.name}
+        width={300}
+        height={300}
+        className=" w-10 h-10"
+      />
+      <div className="my-auto">
+        <a
+          href={track.external_urls.spotify}
+          className="hover:underline line-clamp-1"
+          aria-label={track.name}
+        >
+          {track.name}
+        </a>{" "}
+        <div className="flex gap-1 text-xs text-muted-foreground">
+          <a
+            href={track.album.external_urls.spotify}
+            className="hover:underline line-clamp-1"
+            aria-label={track.album.name}
+          >
+            {track.album.name}
+          </a>
+          /
+          <a
+            href={track.artists[0].external_urls.spotify}
+            className="hover:underline line-clamp-1"
+            aria-label={track.artists[0].name}
+          >
+            {track.artists[0].name}
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default async function MusicTracking() {
   let currentTopTracks, recentTopTracks, allTimeTopTracks;
 
   try {
-    currentTopTracks = await getCurrentTopTracks();
+    currentTopTracks = await getTopTracks("short_term");
+    recentTopTracks = await getTopTracks("medium_term");
+    allTimeTopTracks = await getTopTracks("long_term");
   } catch (error) {
-    console.error(error);
-  }
-
-  try {
-    recentTopTracks = await getRecentTopTracks();
-  } catch (error) {
-    console.error(error);
-  }
-
-  try {
-    allTimeTopTracks = await getAllTimeTopTracks();
-  } catch (error) {
-    console.error(error);
+    console.error("Error fetching top tracks data:", error);
+    return (
+      <div>Oops! Something went wrong while fetching top tracks data.</div>
+    );
   }
 
   return (
     <div className="flex flex-col md:flex-row gap-2">
       <div className="flex flex-col">
-        {currentTopTracks &&
-          currentTopTracks.items &&
-          currentTopTracks.items.map((track: any, index: number) => (
-            <div
-              key={index}
-              className="hover:bg-muted py-1 px-2 transition-all duration-200 "
-            >
-              <div className="flex gap-4">
-                <div className="text-muted-foreground my-auto whitespace-nowrap w-2">
-                  {index + 1}
-                </div>
-                <Image
-                  src={track.album.images[0].url}
-                  alt={track.name}
-                  width={300}
-                  height={300}
-                  className=" w-10 h-10"
-                />
-                <div className="my-auto">
-                  <a
-                    href={track.external_urls.spotify}
-                    className="hover:underline line-clamp-1"
-                    aria-label={track.name}
-                  >
-                    {track.name}
-                  </a>{" "}
-                  <div className="flex gap-1 text-xs text-muted-foreground">
-                    <a
-                      href={track.album.external_urls.spotify}
-                      className="hover:underline line-clamp-1"
-                      aria-label={track.album.name}
-                    >
-                      {track.album.name}
-                    </a>
-                    /
-                    <a
-                      href={track.artists[0].external_urls.spotify}
-                      className="hover:underline line-clamp-1"
-                      aria-label={track.artists[0].name}
-                    >
-                      {track.artists[0].name}
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+        {currentTopTracks?.items?.map((track: any, index: number) => (
+          <Track key={index} track={track} index={index} />
+        ))}
         <p className="text-muted-foreground text-sm text-center py-2 font-mono">
           Top tracks from this week
         </p>
       </div>
 
       <div className="flex flex-col">
-        {recentTopTracks &&
-          recentTopTracks.items &&
-          recentTopTracks.items.map((track: any, index: number) => (
-            <div
-              key={index}
-              className="hover:bg-muted py-1 px-2 transition-all duration-200"
-            >
-              <div className="flex gap-4">
-                <div className="text-muted-foreground my-auto whitespace-nowrap w-2">
-                  {index + 1}
-                </div>
-                <Image
-                  src={track.album.images[0].url}
-                  alt={track.name}
-                  width={300}
-                  height={300}
-                  className=" w-10 h-10"
-                />
-                <div className="my-auto">
-                  <a
-                    href={track.external_urls.spotify}
-                    className="hover:underline line-clamp-1"
-                    aria-label={track.name}
-                  >
-                    {track.name}
-                  </a>{" "}
-                  <div className="flex gap-1 text-xs text-muted-foreground">
-                    <a
-                      href={track.album.external_urls.spotify}
-                      className="hover:underline line-clamp-1"
-                      aria-label={track.album.name}
-                    >
-                      {track.album.name}
-                    </a>
-                    /
-                    <a
-                      href={track.artists[0].external_urls.spotify}
-                      className="hover:underline line-clamp-1"
-                      aria-label={track.artists[0].name}
-                    >
-                      {track.artists[0].name}
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+        {recentTopTracks?.items?.map((track: any, index: number) => (
+          <Track key={index} track={track} index={index} />
+        ))}
         <p className="text-muted-foreground text-sm text-center py-2 font-mono">
           Top tracks from last month
         </p>
       </div>
 
       <div className="flex flex-col">
-        {allTimeTopTracks &&
-          allTimeTopTracks.items &&
-          allTimeTopTracks.items.map((track: any, index: number) => (
-            <div
-              key={index}
-              className="hover:bg-muted py-1 px-2 transition-all duration-200"
-            >
-              <div className="flex gap-4">
-                <div className="text-muted-foreground my-auto whitespace-nowrap w-2">
-                  {index + 1}
-                </div>
-                <Image
-                  src={track.album.images[0].url}
-                  alt={track.name}
-                  width={300}
-                  height={300}
-                  className=" w-10 h-10"
-                />
-                <div className="my-auto">
-                  <a
-                    href={track.external_urls.spotify}
-                    className="hover:underline line-clamp-1"
-                    aria-label={track.name}
-                  >
-                    {track.name}
-                  </a>{" "}
-                  <div className="flex gap-1 text-xs text-muted-foreground">
-                    <a
-                      href={track.album.external_urls.spotify}
-                      className="hover:underline line-clamp-1"
-                      aria-label={track.album.name}
-                    >
-                      {track.album.name}
-                    </a>
-                    /
-                    <a
-                      href={track.artists[0].external_urls.spotify}
-                      className="hover:underline line-clamp-1"
-                      aria-label={track.artists[0].name}
-                    >
-                      {track.artists[0].name}
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+        {allTimeTopTracks?.items?.map((track: any, index: number) => (
+          <Track key={index} track={track} index={index} />
+        ))}
         <p className="text-muted-foreground text-sm text-center py-2 font-mono">
           Top tracks of all time
         </p>
