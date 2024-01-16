2697d34827b23a5f1e6e9f3c2bb8a70c9dbbca25
zpuckeridge <zacchary@puckeridge.me>
2024-01-16 01:37:24 +0000

    feat: robot and sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : "http://localhost:3000";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const routesMap = [""].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...routesMap];
+}
