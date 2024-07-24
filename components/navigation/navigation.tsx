4c8fcd2d8e502d90ce9955cdba36f9f740b08045
zpuckeridge <zacchary@puckeridge.me>
2024-07-24 00:24:36 +0000

    fix: remove prisma call

--- components/navigation/navigation.tsx
+++ components/navigation/navigation.tsx
@@ -1,13 +1,11 @@
 import { getAllPosts } from "@/lib/get-posts";
-import prisma from "@/lib/prisma";
+import { getAllVideos } from "@/lib/get-videos";
 import NavigationClient from "./navigation-client";
 
 export default async function Navigation() {
   const posts = getAllPosts();
 
-  const videos = await prisma.video.findMany({
-    orderBy: { date: "desc" },
-  });
+  const videos = getAllVideos();
 
   return (
     <>
