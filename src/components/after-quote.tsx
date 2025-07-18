830207b2d9cbd9bb5fb3f64a3733a2e6adf66607
zpuckeridge <zacchary@puckeridge.me>
2025-07-18 10:13:56 +1000

    fix: after quote reference styling

--- src/components/after-quote.tsx
+++ src/components/after-quote.tsx
@@ -8,18 +8,18 @@ interface AfterQuoteProps {
 
 const AfterQuote: FC<AfterQuoteProps> = ({ text, subtext, link }) => {
 	return (
-		<div className="text-sm text-right w-full -mt-14">
+		<div className="text-sm text-right w-full -mt-10">
 			<div className="inline-flex items-center gap-2 flex justify-end">
 				<hr className="w-16 border-muted-foreground border-dotted my-auto" />
 				<div>
 					{link ? (
-						<a href={link} className="hover:underline">
+						<a href={link} target="_blank" className="hover:underline">
 							{text}
 						</a>
 					) : (
 						text
 					)}
-					,
+					{subtext && ","}
 				</div>
 				{subtext && <div className="text-muted-foreground">{subtext}</div>}
 			</div>
